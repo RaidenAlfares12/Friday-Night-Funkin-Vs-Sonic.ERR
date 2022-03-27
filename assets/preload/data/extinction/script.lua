175e1ac5c0a715d717e4100d37c1172eb8f90889
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-03-27 12:24:15 +0700

    Update script.lua

--- assets/preload/data/extinction/script.lua
+++ assets/preload/data/extinction/script.lua
@@ -7,3 +7,8 @@ function onStartCountdown()
 	end
 	return Function_Continue;
 end
+function onCreate()
+makeLuaText('watermark', songName .. " | Port By ZeonPlay & Raiden", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
